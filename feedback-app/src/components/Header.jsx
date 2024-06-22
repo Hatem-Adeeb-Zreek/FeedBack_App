d6ac534bd690441f1c09db7eebdffd3138f37f36
Lenovo <hatem.zrek88@gmail.com>
2024-06-22 23:44:47 +0200

    Add some Styles

--- feedback-app/src/components/Header.jsx
+++ feedback-app/src/components/Header.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-function Header({ text }) {
+
+function Header({ text, bgColor, textColor }) {
+  
+  const headerStyles = {
+    backgroundColor: bgColor,
+    color: textColor
+  }  
+
   return (
-    <header>
+    <header style={headerStyles}>
         <div className="container">
             <h1>{text}</h1>
         </div>
     </header>
   )
+}
+
+Header.defaultProps = {
+    bgColor: 'rgba(0,0,0,0.4)',
+    textColor: '#ff6a95'
+}
+
+Header.prototypes = {
+    bgColor: PropTypes.string,
+    textColor: PropTypes.string
 }
 
 export default Header
